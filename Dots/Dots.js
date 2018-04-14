878a19c80f3ca373c57b2fae8f5faeb986d12b5e
Welto <lukas.boschert@freenet.de>
2018-04-14 15:52:45 +0200

    added Instructions

--- Dots/Dots.js
+++ Dots/Dots.js
@@ -1,5 +1,5 @@
 /*
-* athor: Welto
+* author: Welto
 * display random gif from folder
 * > Load JSON with [id:[.gif, solution.img, solution.angle, solution.radius], ...]
 * > chose random id from JSON
@@ -21,6 +21,8 @@ var maxTries;
 var data;
 var num;
 var solved;
+var instru;
+var butInst;
 
 // only works on omnibus server because of coars
 // loadJSON only works on omnibus
@@ -36,6 +38,9 @@ function setup() {
   butt = createButton('submit your guess');
   butt.position(200, height- 90);
   butt.mousePressed(submitGuess);
+  butInst = createButton('Instruction');
+  butInst.position(340, height- 40);
+  butInst.mousePressed(instructions);
   renew = createButton('new riddle');
   renew.position(340, height - 90);
   renew.mousePressed(setGif);
@@ -48,41 +53,43 @@ function setup() {
 }
 
 function draw() {
+  if (instru) return 0;
   image(gif, 0, 0);
 }
 
+function instructions() {
+  instru = true;
+  background(255);
+  text("There are two identical sets of dots to the left and to the right\nwith the exception of exactly one dot.\nLocate that dot (Hint: use cross or parallel view).\nEnter a number from one to twelve corresponding to\nthe clocktime you suspect the extra dot in,\nfollowed bye a small letter \'a\' for outer most ring\'b\' for middle and\n\'c\' for inner ring.\nPress submit to enter your guess.\nPress new riddle to get a fresh set of points.\nCan you guess the right position in only 3 tries?",20,40);
+}
 function setGif() {
   trys = 0;
+  instru = false;
   solved = false;
   num = ceil(random(0.00001, Object.keys(data).length));
-  console.log(num);
   gif = loadGif(data[num].gif);
   fill(255);
   rect(430, height - 100, 370, 50);
   fill(0);
-  console.log(maxTries + " " + trys);
-  text(maxTries - trys + " tries left",  430, height- 60);
+  text(maxTries - trys + " tr" +((maxTries - trys == 1)? "y" : "ies")+ " left", 430, height- 60);
 }
 
 function submitGuess() {
-  if (solved) { return 0;}
+  if (solved || instru) { return 0;}
   guess = inp.value();
   console.log('your Guess: ',guess);
   if (guess.length > 3 || guess.length < 2) {
     console.log('your Guess has invalid length');
-    return;
+    return 0;
   }
   if(guess.length == 2){
     guessAngle = guess[0];
     guessRadius = guess[1];
-    console.log('your guessed angle: ',  guessAngle);
-    console.log('your guessed radius: ',  guessRadius);
   } else{
     guessAngle = guess[0] + guess[1];
     guessRadius = guess[2];
-    console.log('your guessed angle: ',  guessAngle);
-    console.log('your guessed radius: ',  guessRadius);
   }
+  inp.value("");
 
   var solution = true;
   if (abs(guessAngle - data[num].angle/30)%12 > 0.5 + angleTolerance) {
@@ -108,12 +115,12 @@ function submitGuess() {
       // letter doesnt applies to rules
       console.log('your guessed radius has to be in a-c');
       solution = false;
+      return 0;
   }
   fill(255);
   rect(430, height - 100, 370, 50);
   fill(0);
   if (solution) {
-    console.log('you guessed right');
     gif = loadImage(data[num].img);
     solved = true;
     text('you guessed right', 430, height - 60);
@@ -121,11 +128,10 @@ function submitGuess() {
   }
   trys += 1;
   if (trys == maxTries) {
-    console.log('you ran out of tries');
     gif = loadImage(data[num].img);
     solved = true;
     text('you ran out of tries',  430, height - 60);
     return 0;
   }
-  text(maxTries - trys + " tries left",  430, height -60);
+  text(maxTries - trys + " tr" + ((maxTries - trys == 1)? "y" : "ies")+ " left",  430, height -60);
 }
